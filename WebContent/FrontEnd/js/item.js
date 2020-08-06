69e96973ac6d45181bdfff33730b60b88534b6e3
youngjae <jyj960330@gmail.com>
2020-08-06 19:48:26 +0900

    [정영재] 프론트

---
+++ WebContent/FrontEnd/js/item.js
@@ -0,0 +1,10 @@
+var data = {
+    item: [
+        {img: 'image/2.jpg', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/3.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/4.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/5.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/6.jpg', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/10.jpg', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'}
+    ]
+};
