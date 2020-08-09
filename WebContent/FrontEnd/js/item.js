f04de243e0d624fdf74e108bd7a9d21d0f22dc35
Youngjae <jyj960330@gmail.com>
2020-08-09 16:40:38 +0900

    [정영재] 링크 수정

--- WebContent/FrontEnd/js/item.js
+++ WebContent/FrontEnd/js/item.js
@@ -1,6 +1,6 @@
 var data = {
     item: [
-        {img: 'image/2.jpg', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/11.jpg', title: 'O`2nd', desc: '프린팅 패널 탑', price: '298,000'},
         {img: 'image/3.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
         {img: 'image/4.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
         {img: 'image/5.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
