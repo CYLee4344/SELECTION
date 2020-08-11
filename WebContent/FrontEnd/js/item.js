45189b4985b498ed929b936868b90fe736746405
CYLEE <choongyeon4344@gmail.com>
2020-08-11 10:21:10 +0900

    [이충연] bigdata2020-3으로 덮어씀

--- WebContent/FrontEnd/js/item.js
+++ WebContent/FrontEnd/js/item.js
@@ -1,10 +1,10 @@
 var data = {
     item: [
-        {img: 'image/11.jpg', title: 'O`2nd', desc: '프린팅 패널 탑', price: '298,000'},
-        {img: 'image/3.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
-        {img: 'image/4.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
-        {img: 'image/5.png', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
-        {img: 'image/6.jpg', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
-        {img: 'image/10.jpg', title: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'}
+        {img: 'image/11.jpg', brand: 'O`2nd', desc: '프린팅 패널 탑', price: '298,000'},
+        {img: 'image/3.png', brand: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/4.png', brand: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/5.png', brand: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/6.jpg', brand: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'},
+        {img: 'image/10.jpg', brand: 'CLUB MONACO', desc: '드레이프 셔츠 드레스', price: '438,000'}
     ]
 };
